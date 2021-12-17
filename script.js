b12c1661a384a4bc6fe4cdf57cca46d13a841c62
Chris <christopher.yun.3@gmail.com>
2021-12-17 16:04:10 -0500

    set up test folder with some test api calls

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+
+
+
+// Create an app object for namespace 
+
+// add event listners to each options in drop down menu and random dish choice button
+
+// Create a function (getUserInput) to update the variable (userInput) based on user choice and input
+
+// if user select an option other than search by name or random we return a list of dishs to choose from.
+
+// create fucntion to form fetch request with userInput variables.
+
+// create a function to append fatched json data. (append to li that will reflect on DOM)
+
+// create a function to re-set
+
+// create init function and add event listners to it
+
+
 console.log('app.js is connected');
